0e67a0ff9ac42b26604bfe0082ce03d57274444f
Shachar Witkovsky <shachar.witkovsky@sap.com>
2017-11-29 14:11:36 +0200

    fix

--- public/js/ui/gamesPredictions/GamePredictionTile.js
+++ public/js/ui/gamesPredictions/GamePredictionTile.js
@@ -2,16 +2,23 @@ component.GamePredictionTile = (function(){
     var Tile = component.Tile,
         GamePredictionMainTile = component.GamePredictionMainTile;
 
-    return function(props) {
-        var teams = LEAGUE.teams,
-            game = props.game,
-            team1 = teams[game.team1Id],
-            team2 = teams[game.team2Id];
+    return React.createClass({
+        shouldComponentUpdate: function(nextProps) {
+            return this.props.game !== nextProps.game;
+        },
 
-        return re(Tile, {borderLeftColor: team1.color, borderRightColor: team2.color, className: "game-prediction-tile", dialogComponent: "GamePredictionTileDialog", dialogComponentProps:{id: game.id}},
-            re(GamePredictionMainTile, {game: game})
-        );
-    }
+        render: function() {
+            var props = this.props,
+                teams = LEAGUE.teams,
+                game = props.game,
+                team1 = teams[game.team1Id],
+                team2 = teams[game.team2Id];
+
+            return re(Tile, {borderLeftColor: team1.color, borderRightColor: team2.color, className: "game-prediction-tile", dialogComponent: "GamePredictionTileDialog", dialogComponentProps:{id: game.id}},
+                re(GamePredictionMainTile, {game: game})
+            );
+        }
+    });
 })();
 
 
--- public/js/ui/teamsPredictions/TeamPredictionTile.js
+++ public/js/ui/teamsPredictions/TeamPredictionTile.js
@@ -2,21 +2,28 @@ component.TeamPredictionTile = (function(){
     var Tile = component.Tile,
         TeamPredictionMainTile = component.TeamPredictionMainTile;
 
-    return function(props) {
-        var selectedTeam = props.team,
-            borderColor = "gray",
-            team,
-            teamId = selectedTeam.id;
+    return React.createClass({
+        shouldComponentUpdate: function(nextProps) {
+            return this.props.team !== nextProps.team;
+        },
 
-        if (teamId) {
-            team = LEAGUE.teams[teamId];
-            borderColor = team.color;
+        render: function() {
+            var props = this.props,
+                selectedTeam = props.team,
+                borderColor = "gray",
+                team,
+                teamId = selectedTeam.id;
+
+            if (teamId) {
+                team = LEAGUE.teams[teamId];
+                borderColor = team.color;
+            }
+
+            return re(Tile, {borderLeftColor: borderColor, borderRightColor: borderColor, className: "team-prediction-tile", dialogComponent: "TeamPredictionTileDialog", dialogComponentProps: {rank: selectedTeam.rank}},
+                re(TeamPredictionMainTile, {team: team, teamRecords: selectedTeam})
+            );
         }
-
-        return re(Tile, {borderLeftColor: borderColor, borderRightColor: borderColor, className: "team-prediction-tile", dialogComponent: "TeamPredictionTileDialog", dialogComponentProps: {rank: selectedTeam.rank}},
-            re(TeamPredictionMainTile, {team: team, teamRecords: selectedTeam})
-        );
-    }
+    });
 })();
 
 
