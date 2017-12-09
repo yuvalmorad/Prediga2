3d6da9fb4d8c2066d3702e1f97254ed8c2f28e15
Shachar <shacharw6@gmail.com>
2017-12-09 21:55:39 +0200

    fix

--- public/js/ui/leaderBoard/LeaderBoardMainTile.js
+++ public/js/ui/leaderBoard/LeaderBoardMainTile.js
@@ -1,11 +1,12 @@
 component.LeaderBoardMainTile = (function(){
     var BaseMainTile = component.BaseMainTile;
+    var DEAFULT_PROFILE_IMAGE = "../images/default_profile.png";
 
     return function(props) {
         var user = props.user,
             rank = props.rank,
             score = props.score,
-            photo = user.photo,
+            photo = user.photo || DEAFULT_PROFILE_IMAGE,
             name = user.name,
             predictions = user.predictions;
             //strikes = user.strikes,
