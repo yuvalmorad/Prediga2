2599e6d230abf9f95acbcb648021b38e7424e570
Keinang <keinan.gilad@gmail.com>
2017-12-10 21:38:20 +0200

    removing facebook login from the ui

--- public/js/ui/login/LoginPage.js
+++ public/js/ui/login/LoginPage.js
@@ -3,9 +3,9 @@ component.LoginPage = (function(){
         return re("div", { className: "login-page" },
                     re("img", {className: "login-logo", src:"../images/prediga_logo_transparent.png"}),
                     re("div", {className: "login-buttons"},
-                        re("div", {className: "login-button-wrapper"},
+                        /*re("div", {className: "login-button-wrapper"},
                             re("a", {className: "facebook-login", href: "/auth/facebook"}, "Login with Facebook")
-                        ),
+                        ),*/
                         re("div", {className: "login-button-wrapper"},
                             re("a", {className: "google-login", href: "/auth/google"}, "Login with Google")
                         )
