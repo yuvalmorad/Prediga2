90c7658c5a84cd39b630590d22d2afdf7e230dc3
Keinang <keinan.gilad@gmail.com>
2017-12-11 23:09:43 +0200

    Add API for admin to immediate update leaderboard

--- app/controllers/usersLeaderboard.js
+++ app/controllers/usersLeaderboard.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express.Router();
 var UsersLeaderboard = require('../models/usersLeaderboard');
+var UsersLeaderboardService = require('../services/usersLeaderboardService');
 var util = require('../utils/util.js');
 var User = require('../models/user');
 
 app.get('/', util.isLoggedIn, function (req, res) {
     getData().then(function (leaderboardCombined) {
         res.status(200).json(leaderboardCombined);
+    });
+});
+
+app.post('/', util.isAdmin, function (req, res) {
+    UsersLeaderboardService.updateLeaderboard().then(function () {
+        res.status(200).json();
     });
 });
 
