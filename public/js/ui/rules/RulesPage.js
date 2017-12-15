6d4877557b9e50f7096e38dd8720934e2f71cd03
Shachar <shacharw6@gmail.com>
2017-12-15 18:58:37 +0200

    fix

--- public/js/ui/rules/RulesPage.js
+++ public/js/ui/rules/RulesPage.js
@@ -1,6 +1,19 @@
 component.RulesPage = (function(){
     return function RulesPage(props) {
-        return re("div", { className: "" }, "Rules Page place holder");
+        return re("div", { className: "content" },
+            re("div", { className: "scroll-container" },
+                re("h3", {className: "smallMarginBottom"}, "Soccer"),
+
+                re("h5", {}, "90 Minutes Play"),
+                re("p", {className: "smallMarginBottom"}, "text here"),
+
+                re("h5", {}, "Extra-time In-Play Markets"),
+                re("p", {className: "smallMarginBottom"}, "text here"),
+
+                re("h5", {}, "Matches Postponed"),
+                re("p", {className: "smallMarginBottom"}, "text here")
+            )
+        );
     };
 })();
 
