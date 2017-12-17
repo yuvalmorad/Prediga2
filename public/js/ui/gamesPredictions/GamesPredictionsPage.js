dbccd6aa822a28bb879d2a3fcc3eab9af69ae1a8
Shachar Witkovsky <shachar.witkovsky@sap.com>
2017-12-17 17:36:25 +0200

    fix

--- public/js/ui/gamesPredictions/GamesPredictionsPage.js
+++ public/js/ui/gamesPredictions/GamesPredictionsPage.js
@@ -192,7 +192,7 @@ component.GamesPredictionsPage = (function(){
 
             return re("div", { className: "games-prediction-page content hasTilesHeader"},
                 re("div", {className: "tiles-header"},
-                    re(ImageButton, {onClick: this.onPreviousPage, disabled: isLeftButtonDisabled, backgroundPosition: "-18px 0px", backgroundPositionDisabled: "-28px 0px"}),
+                    re(ImageButton, {onClick: this.onPreviousPage, disabled: isLeftButtonDisabled, backgroundPosition: "-19px 0px", backgroundPositionDisabled: "-28px 0px"}),
                     re("div", {className: "title"}, closestPage ? models.leagues.getLeagueName(closestPage.league) + ": " + closestPage.type : ""),
                     re(ImageButton, {onClick: this.onNextPage, disabled: isRightButtonDisabled, backgroundPosition: "0 0", backgroundPositionDisabled: "-10px 0px"})
                 ),
