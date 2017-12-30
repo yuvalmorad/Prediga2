b450cd49aca702e9f33b78f99b98ccce13b4df89
Shachar <shacharw6@gmail.com>
2017-12-30 21:36:38 +0200

    adding some logs for push notifications

--- app/utils/automaticPushNotifications.js
+++ app/utils/automaticPushNotifications.js
@@ -13,12 +13,14 @@ function scheduleJobBeforeGameKickoffTime() {
 
 		const hourBeforeGameKickoffTime = new Date(match.kickofftime);
 		hourBeforeGameKickoffTime.setHours(hourBeforeGameKickoffTime.getHours() - 1);
+		console.log("schedule runAutomaticPushBeforeGame for: ", hourBeforeGameKickoffTime.toString());
 		schedule.scheduleJob(hourBeforeGameKickoffTime, function () {
 			PushSubscription.find({}).then(function (users) {
 				(users || []).forEach(function (user) {
 					MatchPrediction.findOne({matchId: match._id, userId: user.userId}).then(function (matchPrediction) {
 						if (!matchPrediction) {
 							//user didn't fill a match prediction -> push notification for reminder
+                            console.log("sending push notification about not filling prediction for user: ", this.userId);
 							pushNotificationUtil.pushWithSubscription(this, "Please fill your prediction, the game is about to start!!!");
 						}
 					}.bind(user));
