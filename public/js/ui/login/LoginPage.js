d1f441e2acc7935e46d2d289d97b7f253759819a
shachar <shacharw6@gmail.com>
2018-01-12 00:10:50 +0200

    fix

--- public/js/ui/login/LoginPage.js
+++ public/js/ui/login/LoginPage.js
@@ -1,18 +1,27 @@
 window.component = window.component || {};
 component.LoginPage = (function(){
-    return function LoginPage(props) {
-        return re("div", { className: "login-page" },
-                    re("img", {className: "login-logo", src:"../images/prediga_logo_transparent.png"}),
-                    re("div", {className: "login-buttons"},
-                        /*re("div", {className: "login-button-wrapper"},
-                            re("a", {className: "facebook-login", href: "/auth/facebook"}, "Login with Facebook")
-                        ),*/
-                        re("div", {className: "login-button-wrapper"},
-                            re("a", {className: "google-login", href: "/auth/google"}, "Login with Google")
-                        )
-                    )
-        );
-    };
+    return React.createClass({
+        onLoginClicked: function(event) {
+            //workaround for ios - keep the home app after login and don't open safari
+            var target = event.target;
+            window.location = target.getAttribute("href");
+            event.preventDefault();
+        },
+
+        render: function LoginPage(props) {
+           return re("div", { className: "login-page" },
+               re("img", {className: "login-logo", src:"../images/prediga_logo_transparent.png"}),
+               re("div", {className: "login-buttons"},
+                   /*re("div", {className: "login-button-wrapper"},
+                       re("a", {className: "facebook-login", href: "/auth/facebook"}, "Login with Facebook")
+                   ),*/
+                   re("div", {className: "login-button-wrapper"},
+                       re("a", {className: "google-login", href: "/auth/google", traget: "_self", onClick: this.onLoginClicked}, "Login with Google")
+                   )
+               )
+           );
+        }
+    });
 })();
 
 
