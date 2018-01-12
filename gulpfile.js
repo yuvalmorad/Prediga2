fb4f58cd29825153f1f13891e638af87489e0109
shachar <shacharw6@gmail.com>
2018-01-12 07:37:44 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 var Server = require('karma').Server;
 
+var BUILD_FOLDER = "build";
+
 gulp.task('default', function() {
-    gulp.start('buildJS', 'copyFolders');
+    gulp.start('buildJS', 'minify-css', 'copyFolders');
 });
 
 gulp.task('buildJS', function() {
@@ -14,19 +17,25 @@ gulp.task('buildJS', function() {
             js: [ uglify()],
             inlinejs: [ uglify() ]
         }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest(BUILD_FOLDER + '/'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./public/css/*.css')
+        .pipe(cleanCSS(/*{compatibility: 'ie8'}*/))
+        .pipe(gulp.dest(BUILD_FOLDER + '/css'));
 });
 
 gulp.task('copyFolders', function(){
-    var foldersToCopy = ["css", "fonts", "images", "config"];
+    var foldersToCopy = ["fonts", "images", "config"];
     var res = foldersToCopy.map(function(folder){
         return gulp.src(['./public/' + folder + '/**/*'])
-            .pipe(gulp.dest('build/' + folder));
+            .pipe(gulp.dest(BUILD_FOLDER + '/' + folder));
     });
 
     res.push( //copy service worker
         gulp.src(['./public/sw.js'])
-            .pipe(gulp.dest('build'))
+            .pipe(gulp.dest(BUILD_FOLDER))
     );
 
     return res;
