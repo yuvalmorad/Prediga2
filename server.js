fd998c7aa4e64b4d14d0f4a31ad811fd4a5192d4
Keinang <keinan.gilad@gmail.com>
2018-01-31 21:51:15 +0200

    fix

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(express.static(clientFolder));
 app.use(express.static(clientFolder));
 
 // required for passport
