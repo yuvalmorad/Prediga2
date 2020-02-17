fdec33d3b19d43a6e892f77afa21d5d0f26796c3
Gilad Keinan <gilad.keinan@sap.com>
2020-02-17 10:48:31 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,39 +7,43 @@ var Server = require('karma').Server;
 
 var BUILD_FOLDER = "build";
 
-gulp.task('uglify-error-debugging', function () {
-	return gulp.src('./public/index.html')
-	.pipe(usemin({
-		js: [ uglify()],
-		inlinejs: [ uglify() ]
-	}))
-	.on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
-	.pipe(gulp.dest(BUILD_FOLDER + '/'));
+gulp.task('default', function() {
+    gulp.start( 'buildJS', 'minify-css', 'copyFolders');
 });
 
-gulp.task('buildJS', function(done) {
+gulp.task('uglify-error-debugging', function () {
     return gulp.src('./public/index.html')
         .pipe(usemin({
             js: [ uglify()],
             inlinejs: [ uglify() ]
         }))
-	    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
+        .pipe(gulp.dest(BUILD_FOLDER + '/'));
+});
+
+gulp.task('buildJS', function() {
+    return gulp.src('./public/index.html')
+        .pipe(usemin({
+            js: [ uglify()],
+            inlinejs: [ uglify() ]
+        }))
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
         .pipe(gulp.dest(BUILD_FOLDER + '/'));
 
 });
 
-gulp.task('minify-css', function(done) {
+gulp.task('minify-css', function() {
     return gulp.src('./public/css/*.css')
         .pipe(cleanCSS(/*{compatibility: 'ie8'}*/))
-	    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
         .pipe(gulp.dest(BUILD_FOLDER + '/css'));
 });
 
-gulp.task('copyFolders', function(done){
+gulp.task('copyFolders', function(){
     var foldersToCopy = ["fonts", "images", "config"];
     var res = foldersToCopy.map(function(folder){
         return gulp.src(['./public/' + folder + '/**/*'])
-		    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
+            .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err); })
             .pipe(gulp.dest(BUILD_FOLDER + '/' + folder));
     });
 
@@ -48,14 +52,8 @@ gulp.task('copyFolders', function(done){
             .pipe(gulp.dest(BUILD_FOLDER))
     );
 
-    done();
     return res;
 });
-
-gulp.task('default', gulp.series(gulp.parallel('buildJS', 'minify-css', 'copyFolders'), function (done) {
-    // do something
-    done();
-}));
 
 // Run test once and exit
 gulp.task('test', function (done) {
